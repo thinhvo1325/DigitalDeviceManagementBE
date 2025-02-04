58f5fead1baefe3e6f12a99941a050c3e4e02523
Thinh Vo <vocuongthinh33@gmail.com>
2025-02-04 21:06:42 +0700

    first commit

--- api/app/router/db.py
+++ api/app/router/db.py
@@ -23,3 +23,17 @@ async def create_db():
     engine = await get_db()
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+        
+        
+
+@router.post("/new_mapping")
+async def new_mapping(c: int):
+    import subprocess
+    try:
+        cmd = ['alembic revision --autogenerate -m "crate all table"',
+               'alembic upgrade head']
+        result = subprocess.check_output(cmd[c], shell = True, executable = "/bin/bash", stderr = subprocess.STDOUT)
+
+    except subprocess.CalledProcessError as cpe:
+        result = cpe.output
+    return result
